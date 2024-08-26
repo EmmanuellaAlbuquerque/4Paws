4fccea11a63697f481f20c79756c08cee5cd68b8
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-08-26 15:31:25 -0300

    Gera o projeto com Spring Initializr

---
+++ src/test/java/app/com/_paws/ApplicationTests.java
@@ -0,0 +1,13 @@
+package app.com._paws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
