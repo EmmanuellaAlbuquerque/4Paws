5037cfcf7a9b1f46706ace9370613946f4be3029
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-08-28 15:52:23 -0300

    Feat: Adiciona entidade Consulta

---
+++ src/main/java/app/com/_paws/domain/models/Appointment.java
@@ -0,0 +1,34 @@
+package app.com._paws.domain.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity(name = "appointments")
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private LocalDateTime scheduledDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "appointment_veterinarian",
+            joinColumns = @JoinColumn(name = "appointment_id"),
+            inverseJoinColumns = @JoinColumn(name = "veterinarian_id")
+    )
+    private List<Veterinarian> veterinarians;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    private String note;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id")
+    private Service service;
+}
