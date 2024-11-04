97fe29bf06e4f831e344a1c1ad278ec7f0293edd
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-11-04 18:23:20 -0300

    Feat: adiciona configuração de CORS para acesso a API ao projeto frontend em angular

--- src/main/java/app/com/_paws/security/SecurityConfiguration.java
+++ src/main/java/app/com/_paws/security/SecurityConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
