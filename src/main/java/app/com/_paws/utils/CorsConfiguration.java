6fc51a609923b8d7d1943cb2dbaae0717d6fd535
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2025-11-13 08:32:58 -0300

    fix: adiciona expõe header Location para acesso externo da api

--- src/main/java/app/com/_paws/utils/CorsConfiguration.java
+++ src/main/java/app/com/_paws/utils/CorsConfiguration.java
@@ -11,6 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .exposedHeaders("Location");
     }
 }
